27f6e003235d4e77156f8922e6247c087b5c41a5
Andrew Varvel <andy@fastmail.com.au>
2014-09-09 07:58:59 +1000

    Initial commit

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    //MARK: OUTLETS
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let instagramClientID = "8d0f106603304b54bc83cfeb83b08f1f"
+
+    
+    func searchInstagramByHashtag (searchString:String) {
+        println("search button was clicked")
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        let instagramURLString = "https://api.instagram.com/v1/tags/" + searchString + "/media/recent?client_id=" + instagramClientID
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( instagramURLString,
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                
+                if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
+                    self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
+                    for var i = 0; i < dataArray.count; i++ {
+                        let dataObject: AnyObject = dataArray[i]
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            println("image " + String(i) + " URL is " + imageURLString)
                             
+                            let imageView = UIImageView(frame: CGRectMake(0, CGFloat(320*i), 320, 320))
+                            self.scrollView.addSubview(imageView)
+                            imageView.setImageWithURL( NSURL(string: imageURLString))
+                        }                    }
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
+        searchBar.resignFirstResponder()
+        searchInstagramByHashtag(searchBar.text)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.searchBar.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Photo Search Example/bridge.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  Photo Search Example
+//
+//  Created by ANDREW VARVEL on 9/09/2014.
+//  Copyright (c) 2014 Andrew Varvel. All rights reserved.
+//
+
+#import "AFNetworking.h"
+#import "UIKit+AFNetworking.h"
+
+
